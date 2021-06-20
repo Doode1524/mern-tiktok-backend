711e9a3521970f18c39df8f01cbf05f3740eaa5d
joey <doode1524@gmail.com>
2021-06-20 17:12:15 -0400

    server.js init

--- README.md
+++ README.md
@@ -3,5 +3,9 @@ entry point server.js
 add script::
 "start": node server.js
 npm i express mongoose
+server.js file
+add "type": "module"  // for es6
+npm i -g nodemon
+nodemon server.js in terminal
 
 # mern-tiktok-backend
---
+++ server.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+app.listen(port, () => console.log(`Listening on localhost:${port}`))
