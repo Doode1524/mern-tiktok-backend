8919f99c8cb89010c7f18b425f3c48ad9f6cc203
joey <doode1524@gmail.com>
2021-06-20 18:28:19 -0400

    import data

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 import express from "express";
-
+// app config
 const app = express();
 const port = 9000;
+// middlewares
 
+// db config
+
+// api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World"));
-
+// listener
 app.listen(port, () => console.log(`Listening on localhost:${port}`))
+
+
